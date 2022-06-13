fd3bdbad9ee98904013667ad5be1d2bc47f6a116
GeorgeSharikov <75853792+GeorgeSharikov@users.noreply.github.com>
2022-06-13 20:26:54 +0300

    Update dbConnection.js

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -2,14 +2,25 @@ import {Sequelize} from "sequelize";
 import env from 'dotenv'
 env.config()
 
-export const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        logging: false
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
-)
+}
+//       new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         dialect: 'postgres',
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//         logging: false
+//     }
+// )
